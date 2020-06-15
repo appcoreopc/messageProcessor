074016ed5b5d67ac3591aaf8beeb56d0c0f4e1d8
jeremy <jeremy@jeremys-MacBook-Pro.local>
2020-06-15 22:41:38 +0800

    Update project files

--- MessageProcessor/MessageProcessorFunction/MessageProcessorFunction.cs
+++ MessageProcessor/MessageProcessorFunction/MessageProcessorFunction.cs
@@ -1,8 +1,17 @@
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Logging;
+
 namespace MessageProcessor
 {
     public static class MessageProcessorFunction
     {
-        [FunctionName("Function1")]
+        [FunctionName("MessageProcessorFunction")]
         public static void Run([ServiceBusTrigger("myqueue", Connection = "")]string myQueueItem, ILogger log)
         {
             log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
